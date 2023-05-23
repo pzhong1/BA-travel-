e590d49464bce3ed2556c29554ffe4303d4171d0
puze zhong <zpz9991@gmail.com>
2023-05-23 15:49:54 -0500

    created  second html file and edit weather API

--- assets/script.js
+++ assets/script.js
@@ -1,58 +1,76 @@
 
 
-$(document).ready(function() { // using ready() method to warp all the codes to make sure the HTML is fully loaded before running any other codes 
-  var apiKey = "75c5af5c353621d37f558f040992d1fd"; // puze zhong's openweather API key for weather info
+$(document).ready(function () { // using ready() method to warp all the codes to make sure the HTML is fully loaded before running any other codes 
+  var apiKey = "e9fca33cb368be842d7fa15031d8b7e5"; // puze zhong's openweather API key for weather info
+
 
   function loadData(city) {
-    var weatherApiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey; // openweather webside  this is for when user enter city then the server side API will display the weather info, it require personal API key which i included above
+    var weatherApiUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + apiKey;// openweather webside  this is for when user enter city then the server side API will display the weather info, it require personal API key which i included above
 
-      $.ajax({ // im using this function becuase it it can exchange data with a server, update parts of the page without reloading the whole page.
-        url: weatherApiUrl, // set URL to request weatherApiurl to captch data
-        method: 'GET', //i need use GET method to request data from weather URL
-        success: function(response) { // Set a function that will be called when the request succeeds
-          var temperatureC = response.main.temp - 273.15; // Convert Kelvin to Celsius
-          var temperatureF = temperatureC * 9/5 + 32; // Convert Celsius to Fahrenheit
-  
-          var weatherDescription = response.weather[0].description; // stores the weather description from the server's response into the weatherDescription variable. array[0] stand for the first element
-  
-  
-          // creat a HTML string(h2) this is use to display eather infomation 
-          var weatherInfoHtml = '<h2>Weather Information</h2>';
-          weatherInfoHtml += '<p>Temperature: ' + temperatureF.toFixed(2) + '°F</p>'; // Show temperature up to 2 decimal places im using Fahrenheit instead of Celsius
-          weatherInfoHtml += '<p>Description: ' + weatherDescription + '</p>'; // weather description from the weather website
-  
-          $('#weather-info').html(weatherInfoHtml); //Use jQuery here to  overwrite the existing content of that element.
-  
-        }, // closing success
-  
-        error: function(error) { // if the ajax function fails then return error
-          console.log('Error:', error); // will need inspect if i want test my code and see this console.log msg
-        }// closing error()
+    $.ajax({ // im using this function becuase it it can exchange data with a server, update parts of the page without reloading the whole page.
+      url: weatherApiUrl, // set URL to request weatherApiurl to captch data
+      method: 'GET', //i need use GET method to request data from weather URL
+      success: function (answer) { // Set a function that will be called when the request succeeds
 
-      });//closing ajax()
+
+
+        // creat a HTML string(h2) this is use to display weather infomation 
+        var weatherInfoHtml = '<h2>Weather for ' + city + '</h2>'; // weather for any city that user entered  will display at FlyDrvie page 
+
+        for (var i = 0; i < answer.list.length; i += 7) { // for loop  i have 5 days weather so i set i += 7 so the code for weather only display only once
+          var date = new Date(answer.list[i].dt * 1000);  // .dt(data receiving time)  *1000 to obtain timestamp to date format
+          var temperatureC = answer.list[i].main.temp - 273.15;  //-273.15 so i can get Celsius
+          var temperatureF = temperatureC * 9 / 5 + 32;// use this formula to change to from C to Fahrenheit
+          var weatherDescription = answer.list[i].weather[0].description;// description for weather infomation
+          var weatherIcon = answer.list[i].weather[0].icon; // Get the weather icon ID
+
+          // Create the image element for the weather icon // this url is found at "openweathermap.org/weather-conditions" website 
+          var weatherIconUrl = "http://openweathermap.org/img/wn/" + weatherIcon + ".png"; // create a icon for weather status // https://openweathermap.org/weather-conditions this is icon webURL and then i want my icon display in .png
+          var weatherIconHtml = '<img src="' + weatherIconUrl + '" alt="' + weatherDescription + '">';// insert the icon
+
+
+          weatherInfoHtml += '<h3>' + date.toLocaleDateString() + '</h3>'; //create a h3 element and add the data info for display in web page and use toLocalDateString() to formatted the date information
+          weatherInfoHtml += '<p>Temperature: ' + temperatureF.toFixed(0) + '°F' + weatherIconHtml + '</p>'; // temperature info will display in F  and i set tofix(0) so i can get a whole nume for temperture  in stead of getting any decimal  
+          weatherInfoHtml += '<p>Weather status: ' + weatherDescription + '</p>'; // create a p element for weather status  display 
+        } //for loop
+
+        $('#weather-info').html(weatherInfoHtml);
+        localStorage.setItem('weatherInfo', weatherInfoHtml);// stores the weather information HTML  in the browser local storage so the info will display when page is load
+
+
+        // set time when i click the search button at home page then it will take i sec and then load to the other page 
+        setTimeout(function () {
+          window.location.href = './assets/FlydriveWeather.html';
+        }, 100);  //100 = 1sec
+      },// closing success 
+
+
+      error: function (error) { // if the ajax function fails then return error
+        console.log('Error:', error); // will need inspect if i want test my code and see this console.log msg
+      }// closing error()
+
+    });//closing ajax()
 
   } // closing loadData()
 
-  
-  $('#search-btn').click(function() { // click function that link to id search-btn 
+
+  $('#search-btn').click(function (event) { // click function that link to id search-btn 
+    event.preventDefault();
     console.log("Button clicked"); // im just testing make sure my click function is working //after testing it is working 
     var city = $('#city-input').val(); // add val() method to id city-input and gave it a var name city so when input random city the brower will remember the cityName  user just entered
-    if (city !== " ") { // city input can't not be empty. if is not empty then call the function loadData
+    if (city !== "") { // city input can't not be empty. if is not empty then call the function loadData
       loadData(city); // Call your function here
     }// closing if statement
   }); // closing click fucntion
 
+  ///////////////  END OF OpenWeather API ///////////////////////////////
 
 
 
-  
 
 
-    // Hotel info 
-   
 
-    // Local attractions and ratings info 
-    
+
 
 
 });// closing ready() method
